92754178b5602863f92ee2280abcde1c5488e84a
Ubuntu <ubuntu@ip-172-31-15-87.ap-south-1.compute.internal>
2025-08-29 10:29:47 +0000

    terraform file

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    ami           = "ami-02d26659fd82cf299"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
 }
